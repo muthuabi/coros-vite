7129e67aa8c60ad004741d32b4591d76df267430
muthuabi <muthuabi292@gmail.com>
2025-03-19 00:31:02 +0530

    Server setting UP for API just a start

--- room-server/src/server.js
+++ room-server/src/server.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+const express=require("express");
+const app=express();
+const denv=require("dotenv");
+denv.config();
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT,(svr)=>{
+    console.log(`Server is running on port ${PORT}`);
+});
